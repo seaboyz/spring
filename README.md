4440a6124036ddbbdf23da0f62049b32d635c92e
seaboyz <seaboyz@hotmail.com>
2022-05-10 13:51:26 -0500

    Add starter code

---
+++ README.md
@@ -0,0 +1,1 @@
+### This is the learning project for springboot.
---
+++ src/main/java/com/webdev/springrestapi/SpringRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.webdev.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestapiApplication.class, args);
+	}
+
+}
